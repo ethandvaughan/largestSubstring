6aa20e94a1087bfde980cfbaf5abd249bed65bed
Ethan Vaughan <ethandvaughan@gmail.com>
2021-12-06 09:48:50 -0800

    Add phylogenetic distance

--- largestSubstring.py
+++ largestSubstring.py
@@ -1,11 +1,11 @@
 import sys
 
 # Instantiate globals
-firstString = sys.argv[1]
-secondString = sys.argv[2]
+firstString = sys.argv[1].lower()
+secondString = sys.argv[2].lower()
 substring = ""
 substrings = []
-sep = 0
+
 
 # Looks through the strings and adds the largest substring to list substrings
 # i: index for firstString
@@ -14,7 +14,6 @@ def findSubstring(i, j):
 	global substring 
 	global firstString
 	global secondString
-	global sep
 	
 	# Catches index out of range if the last letter of a string matches
 	try:
@@ -22,25 +21,39 @@ def findSubstring(i, j):
 			substring = substring + firstString[i]
 			return(findSubstring(i+1, j+1))
 		else:
-			substrings.append(substring + " " + str(sep))
+			substrings.append(substring)
 			substring = ""
 			return 
 	except:
-		substrings.append(substring + " " + str(sep))
+		substrings.append(substring)
 		substring = ""
 		return
+
+
+# Calculates the phylogenetic distance if the strings are of equal length
+def getDistance():
+	global firstString
+	global secondString
+	if len(firstString) != len(secondString):
+		return("Strings are not of the same length")
+
+	same = 0
+	for i in range(len(firstString)):
+		if (firstString[i] == secondString[i]):
+			same += 1
+	return(len(firstString) - same)	
+
 	
 # Starter method that finds the first matching letters
 def start():
 	global firstString
 	global secondString
 	global substrings
-	global sep
 	for i in range(len(firstString)):
 		for j in range(len(secondString)):
 			if firstString[i] == secondString[j]:
-				sep = abs(i - j)
 				findSubstring(i, j)
+
 
 # Returns the largest element of the list of substrings
 def largest():
@@ -60,6 +73,11 @@ def main():
 	firstString = firstString[::-1]
 	secondString = secondString[::-1]
 	start()
+
 	print(largest())
+
+	# Calculate distance
+	secondString = secondString[::-1]
+	print("Phylogenetic Distance: ", getDistance())
 
 main()
